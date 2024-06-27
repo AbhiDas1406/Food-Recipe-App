d5e66102d0266c2630d37d4b48c5469c75736a8e
AbhiDas1406 <abhidaskgpian@gmail.com>
2024-06-27 15:57:20 +0530

    corrected the spelling fovourites

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
-import Favorites from "./pages/favorites";
+import Favorites from "./pages/favourites";
 import Details from "./pages/details";
 
 function App() {
